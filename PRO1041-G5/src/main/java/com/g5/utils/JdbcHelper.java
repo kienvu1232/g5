a93bb14bd73a192b33e9a6a8ca1bfaf829630cf6
cuong130506 <cuongdo6926@gmail.com>
2023-11-17 12:55:27 +0700

    no message

--- PRO1041-G5/src/main/java/com/g5/utils/JdbcHelper.java
+++ PRO1041-G5/src/main/java/com/g5/utils/JdbcHelper.java
@@ -5,9 +5,9 @@ import java.sql.*;
 public class JdbcHelper {//Lớp này giải quyết kết nối xử lý truy vấn
 
     private static String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-    private static String connectionSql = "jdbc:sqlserver://localhost:1433;databaseName=asm";
-    private static String acc = "huypn";
-    private static String pass = "huypn";
+    private static String connectionSql = "jdbc:sqlserver://localhost:1433;databaseName=PTPM_FINALLY_JAVA_SOF3022";
+    private static String acc = "sa";
+    private static String pass = "123";
 
     private static Connection conn;
 
